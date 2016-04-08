b66ed5dc227d27eac4ada9b02d7c29b5e276bc3b
16819749 <16819749@qq.com>
2016-04-08 17:11:09 +0800

    YHESE

---
+++ obj/local/armeabi/objs-debug/hello-jni/hello-jni.o.d
@@ -0,0 +1,73 @@
+obj/local/armeabi/objs-debug/hello-jni/hello-jni.o: jni/hello-jni.c \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/string.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/cdefs.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/cdefs_elf.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/android/api-level.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/malloc.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/jni.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/stdio.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/types.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/stdint.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/_types.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/machine/_types.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/_wchar_limits.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/posix_types.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/stddef.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/compiler.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/asm/posix_types.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/asm/types.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/types.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/machine/kernel.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/sysmacros.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/stdlib.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/alloca.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/strings.h \
+ I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/memory.h
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/string.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/cdefs.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/cdefs_elf.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/android/api-level.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/malloc.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/jni.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/stdio.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/types.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/stdint.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/_types.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/machine/_types.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/_wchar_limits.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/posix_types.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/stddef.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/compiler.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/asm/posix_types.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/asm/types.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/linux/types.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/machine/kernel.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/sys/sysmacros.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/stdlib.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/alloca.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/strings.h:
+
+I:/Android2016/03Tools/02run/01ndk/android-ndk-r10e/platforms/android-16/arch-arm/usr/include/memory.h:
---
+++ src/com/example/hellojni/HelloJni.java
@@ -0,0 +1,71 @@
+
+package com.example.hellojni;
+
+import android.app.Activity;
+import android.widget.TextView;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class HelloJni extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_1);
+		/*
+		 * Create a TextView and set its content. the text is retrieved by
+		 * calling a native function.
+		 */
+		//lc_doexcute1(null);
+
+	}
+
+	public void lc_doexcute1(View v) {
+		TextView tv ;
+		tv = (TextView) this.findViewById(R.id.textView1);
+		String s=stringFromJNI();
+		//tv.setText(s);
+		 
+		 nativeCallBack();
+	}
+	
+	
+	public void callback_func(int notify_id, int param) 
+    { 
+        Log.i("JNICallback","notify_id="+notify_id+";param="+param); 
+        TextView tv ;
+		tv = (TextView) this.findViewById(R.id.textView1);
+		tv.setText(tv.getText() + ","+ param);
+    } 
+    public native void nativeCallBack();
+	
+
+	/*
+	 * A native method that is implemented by the 'hello-jni' native library,
+	 * which is packaged with this application.
+	 */
+	public native String stringFromJNI();
+
+	/*
+	 * This is another native method declaration that is *not* implemented by
+	 * 'hello-jni'. This is simply to show that you can declare as many native
+	 * methods in your Java code as you want, their implementation is searched
+	 * in the currently loaded native libraries only the first time you call
+	 * them.
+	 *
+	 * Trying to call this function will result in a
+	 * java.lang.UnsatisfiedLinkError exception !
+	 */
+	public native String unimplementedStringFromJNI();
+
+	/*
+	 * this is used to load the 'hello-jni' library on application startup. The
+	 * library has already been unpacked into
+	 * /data/data/com.example.hellojni/lib/libhello-jni.so at installation time
+	 * by the package manager.
+	 */
+	static {
+		System.loadLibrary("hello-jni");
+	}
+}
